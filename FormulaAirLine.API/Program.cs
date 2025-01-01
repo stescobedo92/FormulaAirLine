f7e21c7ed011f169ed87275c7203cdf771d31a76
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-01 16:38:33 -0600

    Add controller support and register IMessageProducer in the service container

--- FormulaAirLine.API/Program.cs
+++ FormulaAirLine.API/Program.cs
@@ -1,9 +1,14 @@
+using FormulaAirLine.API;
+using FormulaAirLine.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IMessageProducer, MessageProducer>();
 
 var app = builder.Build();
 
