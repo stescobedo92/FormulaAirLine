d8b50c8261efa18d9c209a9b25769e2b3101f1ac
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2025-01-01 23:32:41 -0600

    Implement RabbitMQ connection and message consumption for ticket processing

--- FormulaAirLine.TicketProcessing/Program.cs
+++ FormulaAirLine.TicketProcessing/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Welcome to the ticketing service");
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest",
+    VirtualHost = "/"
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare(queue: "bookings", durable: true, exclusive: true);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"New ticket processing is initiated for - {message}");
+};
+
+channel.BasicConsume(queue: "bookings", autoAck: true, consumer: consumer);
